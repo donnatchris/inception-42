e0b0bb35f660278f3f8858dc05e5b820b8330968
donnatchris <donnatchris@live.fr>
2025-07-22 15:11:39 +0200

    ADD wordpress ok

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [ ! -f wp-config.php ]; then
-	echo "Creating wp-config.php..."
+# waiting for MariaDB
+until mysqladmin ping -h"mariadb" -u"$MDB_USER" -p"$MDB_USER_PASS" --silent; do
+  echo "⏳ Waiting for MariaDB to be ready..."
+  sleep 2
+done
 
+if [ ! -f wp-config.php ]; then
+
+	echo "Creating wp-config.php..."
 	wp config create \
 		--dbname="$MDB_NAME" \
 		--dbuser="$MDB_USER" \
@@ -10,33 +16,23 @@ if [ ! -f wp-config.php ]; then
 		--dbhost="mariadb" \
 		--path=/var/www/wordpress \
 		--allow-root
-
 	wp core install \
 		--url="$DOMAIN_NAME" \
 		--title="$WEBSITE_TITLE" \
 		--admin_user="$WP_ADMIN_LOGIN" \
-		--admin_password="$WP_ADMIN_PASSWORD" \
+		--admin_password="$WP_ADMIN_PASS" \
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--skip-email \
 		--allow-root
 
 	echo "Creating wp user..."
-
 	wp user create "$WP_USER_LOGIN" "$WP_USER_EMAIL" \
 		--role=author \
-		--user_pass="$WP_USER_PASSWORD" \
+		--user_pass="$WP_USER_PASS" \
 		--allow-root
 
-	echo "WordPress installtion ended."
+	echo "WordPress installation ended."
 
-	echo "Configuring redis..."
-	wp config set WP_REDIS_HOST redis --allow-root
-  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
- 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
- 	wp config set WP_REDIS_CLIENT phpredis --allow-root
-	wp plugin install redis-cache --activate --allow-root
-    wp plugin update --all --allow-root
-	wp redis enable --allow-root
 else
 	echo "WordPress is already configured."
 fi
